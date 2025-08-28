7b6a0363c55f65c1ee1c865ca8f1875b888d4f41
Gowreesh10 <gowreeshcse10@gmail.com>
2025-08-29 02:26:25 +0530

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bingo Game (Unity + C#/.NET)
+A clean-architecture Bingo game project demonstrating OOP, state machines, events, async/await, LINQ, 
+design patterns, reflection, persistence, and unit testing.
---
+++ src/Infrastructure/Rules/Dynamic/RuleLoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo.src.Infrastructure.Rules.Dynamic
+{
+    internal class RuleLoader
+    {
+    }
+}

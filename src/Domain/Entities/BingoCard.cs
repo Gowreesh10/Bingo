1b7f2d69098caae489f02cdd91b90fc07fd3ff8d
Gowreesh10 <gowreeshcse10@gmail.com>
2025-08-30 01:24:36 +0530

    feat: Add bingo card entity

--- src/Domain/Entities/BingoCard.cs
+++ src/Domain/Entities/BingoCard.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bingo.src.Domain.Entities
 {
-    internal class BingoCard
+    public class BingoCard
     {
+        private readonly BingoNumber[,] _grid;
+
+        public int Rows => _grid.GetLength(0);
+        public int Columns => _grid.GetLength(1);
+
+        public BingoCard(int[,] numbers)
+        {
+            int rows = numbers.GetLength(0);
+            int cols = numbers.GetLength(1);
+
+            _grid = new BingoNumber[rows, cols];
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    _grid[r, c] = new BingoNumber(numbers[r, c]);
+                }
+            }
+        }
+
+        public void MarkNumber(int value)
+        {
+            foreach (var number in _grid)
+            {
+                if (number.Value == value)
+                {
+                    number.Mark();
+                }
+            }
+        }
+
+        public void UnmarkNumber(int value)
+        {
+            foreach (var number in _grid)
+            {
+                if (number.Value == value)
+                {
+                    number.Unmark();
+                }
+            }
+        }
+
+        public BingoNumber GetNumber(int row, int col) => _grid[row, col];
     }
 }
